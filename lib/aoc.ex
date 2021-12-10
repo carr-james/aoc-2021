b00b19e9675c9f25f8796635a954955dee4d8cab
James Carr <james.carr@iconsolutions.com>
2021-12-10 13:24:07 +0000

    day 10 - part 1

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -1,6 +1,6 @@
 defmodule Aoc do
   def main(_args \\ []) do
-    input = Aoc.Input.get_lines("input/day9.txt")
-    Aoc.Day9.solve(input)
+    input = Aoc.Input.get_lines("input/day10.txt")
+    Aoc.Day10.solve(input)
   end
 end
--- lib/solutions/day10.ex
+++ lib/solutions/day10.ex
@@ -1,5 +1,33 @@
 defmodule Aoc.Day10 do
   def solve(input) do
-
+    Enum.map(input, &String.split(&1, "", trim: true))
+    |> Enum.map(&find_corrupted/1)
+    |> Enum.filter(fn a -> not Enum.empty?(a) end)
+    |> Enum.map(fn [error] -> syntax_error_score_map[error] end)
+    |> Enum.sum()
+    |> IO.inspect()
   end
+
+  defp find_corrupted(chunks), do: find_corrupted(chunks, Stack.new)
+  defp find_corrupted([], _), do: []
+  defp find_corrupted([head | tail] = _chunks, stack) do
+    case head in openers do
+      true ->
+        stack = Stack.push(stack, head)
+        find_corrupted(tail, stack)
+      false ->
+        {popped, stack} = Stack.pop(stack)
+        case popped == closer_to_opener_map[head] do
+          true -> find_corrupted(tail, stack)
+          _ -> [head]
+        end
+    end
+  end
+
+  defp openers, do: ["(", "[", "<", "{"]
+  defp closers, do: [")", "]", ">", "}"]
+  defp opener_to_closer_map, do: Enum.into(Enum.zip(openers(), closers()), %{})
+  defp closer_to_opener_map, do: Enum.into(Enum.zip(closers(), openers()), %{})
+  defp syntax_error_score_map, do: %{")" => 3, "]" => 57, "}" => 1197, ">" => 25137}
+
 end
